49af6a331b91d4b58351d97fd86ee0c2a5581708
Vitor Paz <101137538+Viipaxx@users.noreply.github.com>
2023-06-05 12:06:55 -0300

    Atualizações no Header

--- assets/js/modoDark.js
+++ assets/js/modoDark.js
@@ -1,14 +1,16 @@
-const t = document.querySelector('.modo-dark')
+const darkMode = document.querySelector('#modo-dark')
 
-t.addEventListener('click', () => {
+darkMode.addEventListener('click', () => {
     const imgIcon = document.querySelector('#modo-img')
     const darkLight = document.querySelector('#darkLight')
 
-    t.classList.toggle('teste')
     darkLight.classList.toggle('dark')
     darkLight.classList.toggle('light')
     
     imgIcon.classList.toggle('fa-moon')
     imgIcon.classList.toggle('fa-sun')
+
+    darkMode.classList.toggle('modo-light')
+    darkMode.classList.toggle('modo-dark')
 
 })
