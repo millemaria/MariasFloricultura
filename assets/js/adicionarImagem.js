b44a8a63b32d6d58b119b638e90e460e936e7df1
Karolyna <karolrolimpaz10@gmail.com>
2023-06-10 01:46:35 -0300

    começando a tela de dados do usuario

---
+++ assets/js/adicionarImagem.js
@@ -0,0 +1,6 @@
+let img = document.getElementById("img");
+let input = document.getElementById("input");
+
+input.onchange = (e) => {
+  if (input.files[0]) img.src = URL.createObjectURL(input.files[0]);
+};
