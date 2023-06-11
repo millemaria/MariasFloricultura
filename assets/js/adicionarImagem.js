880bd11b519deca2ecc2029c49e60b8365ff6615
Karolyna <karolrolimpaz10@gmail.com>
2023-06-11 18:36:35 -0300

    Criação da tela de Dados do Usuário

--- assets/js/adicionarImagem.js
+++ assets/js/adicionarImagem.js
@@ -1,5 +1,5 @@
-let img = document.getElementById("img");
-let input = document.getElementById("input");
+let img = document.getElementById("img-user");
+let input = document.getElementById("foto");
 
 input.onchange = (e) => {
   if (input.files[0]) img.src = URL.createObjectURL(input.files[0]);
